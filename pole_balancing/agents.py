32a94650ad6ba29a6898ff091a4c6020e037ce41
shashank <shashank.f1@gmail.com>
2016-09-06 14:11:25 -0700

    method to run the game

--- pole_balancing/agents.py
+++ pole_balancing/agents.py
@@ -2,7 +2,7 @@ import logging
 import os, sys
 import abc
 import gym
-
+import numpy as np
 
 gym.scoreboard.api_key = "sk_hJMbMlYzSpSsucakOMOULg"
 
@@ -28,7 +28,7 @@ class GreedyMemoryAgent(SequenceAgent):
     This Agent models the input (state space) as a sequence and 1/0 as the output.
     It is called MemoryAgent because it remembers its history to take better decisions in future
     """
-    def __init__(self, env, history=500):
+    def __init__(self, env, history=50000):
 
         # Environment and Action Space
         self.env = env
@@ -79,18 +79,12 @@ class GreedyMemoryAgent(SequenceAgent):
     def policy(self):
 
         from sklearn.neighbors import KNeighborsRegressor
-        from sklearn.manifold import TSNE
-        import numpy as np
 
         # Models used
-        tsne = TSNE(perplexity=20, n_components=1, init='pca', n_iter=1000)
         classifier = KNeighborsRegressor(n_neighbors=2)
 
-        # low dimension embedding of observation
-        univariate_obs = tsne.fit_transform(self.past_obs)
-
         # classifier
-        classifier_input = np.array([list(e) for e in zip(univariate_obs, self.past_action)])
+        classifier_input = np.array([[*e[0], e[1]] for e in zip(self.past_obs, self.past_action)])
 
         classifier_output_lst = []
         for episode, episode_range in enumerate(self.value_function):
@@ -98,11 +92,11 @@ class GreedyMemoryAgent(SequenceAgent):
                 classifier_output_lst.append(episode_range - i - 1)
         classifier_output = np.array(classifier_output_lst)
         classifier.fit(classifier_input, classifier_output)
-        return univariate_obs[-1], classifier
+        return self.past_obs[-1], classifier
 
-    def act(self, last_observation, last_action, done, policy='greedy'):
+    def act(self, last_observation, last_action, done, policy='e-greedy'):
 
-        if((last_action!=None) and (policy is 'greedy')):
+        if(last_action!=None):
 
             # Push last event
             self.store_event(last_observation, last_action, done)
@@ -115,20 +109,22 @@ class GreedyMemoryAgent(SequenceAgent):
             if(self.history_full()):
                 self.history_pop()
 
-            try:
-                # Compute Policy
-                information_state, current_policy = self.policy()
+            # Compute Policy
+            information_state, current_policy = self.policy()
 
-                # Apply greedy method
-                expected_reward = {}
-                for action in range(self.action_space.__dict__['n']):
-                    expected_reward[action] = current_policy.predict([[information_state, action]])[0]
+            # Calculate Expected Reward
+            expected_reward = {}
+            for action in range(self.action_space.__dict__['n']):
+                expected_reward[action] = current_policy.predict([[*information_state, action]])[0]
+
+            # Use policy for action selection
+            if(policy=='greedy'):
                 return (k for k in sorted(expected_reward, key=expected_reward.get, reverse=True)).__next__()
-
-            except:
-                logger.error(sys.exc_info()[0])
-                return self.action_space.sample()
-
+            else:
+                if(np.random.random()<0.1):
+                    return self.action_space.sample()
+                else:
+                    return (k for k in sorted(expected_reward, key=expected_reward.get, reverse=True)).__next__()
         else:
             return self.action_space.sample()
 
@@ -141,59 +137,36 @@ class RandomAgent(object):
     def act(self, observation, reward, done):
         return self.action_space.sample()
 
-if __name__ == '__main__':
+
+def run_game(agent_type):
+
     env = gym.make('CartPole-v0')
     agent_name = 'random_agent'
     outdir = '/tmp/'+agent_name+'_results'
     env.monitor.start(outdir, force=True, seed=0)
+    episode_count = 300
+    max_steps = 200
 
-    select_agent = 'greedy'
-
-    if(select_agent is 'random'):
-        agent = RandomAgent(env.action_space)
-        episode_count = 500
-        max_steps = 200
-        reward = 0
-        done = False
-        print("action space = {} and size = {}".format(env.action_space, env.action_space.__dict__))
-        sys.exit(-1)
-        for i in range(episode_count):
-            ob = env.reset()
-
-            for j in range(max_steps):
-                action = agent.act(ob, reward, done)
-                ob, reward, done, _ = env.step(action)
-                if done:
-                    break
-
-        logger.info("Successfully ran {}. Now uploading results to the scoreboard".format(agent))
-
-    elif(select_agent is 'greedy'):
-
-        print('running env with agent = {}'.format('greedy'))
-
+    if(agent_type is 'random'):
+        agent = RandomAgent()
+    else:
         agent = GreedyMemoryAgent(env)
-        episode_count = 500
-        max_steps = 200
-        reward = 0
 
-        for i in range(episode_count):
+    for i in range(episode_count):
+        # Initial Values
+        ob = env.reset()
+        reward = 0.0
+        done = False
+        last_action = None
 
-            last_ob = env.reset()
-            last_action = None
-            done = False
+        for j in range(max_steps):
+            action = agent.act(ob, reward, done, last_action)
+            ob, reward, done, _ = env.step(action)
+            last_action = action
+            if done:
+                break
 
-            for j in range(max_steps):
-
-                if done:
-                    break
-
-                action = agent.act(last_ob, last_action, done)
-                ob, reward, done, _ = env.step(action)
-                last_action = action
-                last_ob = ob
-
-        logger.info("Successfully ran {}. Now uploading results to the scoreboard".format(agent))
-
+    logger.info("Successfully ran {}. Now uploading results to the scoreboard".format(agent))
     env.monitor.close()
     gym.upload(outdir)
+    return agent
